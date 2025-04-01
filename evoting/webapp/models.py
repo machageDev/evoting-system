1759cf99e792197a53d74ac19d85e606e629e24b
machageDev <machagefranklyn@gmail.com>
2025-03-31 20:16:38 -1200

    voter model

--- evoting/webapp/models.py
+++ evoting/webapp/models.py
@@ -33,33 +33,16 @@ class Candidate(models.Model):
     def __str__(self):
         return f"{self.name} - {self.position}"
     
-# Custom Manager for Voter
-class VoterManager(UserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        
-        return self.create_user(email, password, **extra_fields)
-
-# Custom Voter model extending AbstractUser
 class Voter(models.Model):
     username = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True, max_length=254)
+    
+    # ForeignKey to Django's built-in User model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    user = models.ForeignKey(User,on_delete=models.CASCADE,default = None)
     def __str__(self):
         return self.email
-
 
 
 class Vote(models.Model):
